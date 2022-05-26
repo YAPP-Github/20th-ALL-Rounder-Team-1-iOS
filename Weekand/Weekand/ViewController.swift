5673145c453622555b6db0aa425f55f9db7e90a7
Daegun Choi  <chcs1370@gmail.com>
2022-05-26 10:19:47 +0900

    Revert "Welcome Page & SignIn page 구현, CustomView init 추가, WDefaultButton 스타일 리팩토링, CheckBox & TextButton 추가"

---
+++ Weekand/Weekand/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weekand
+//
+//  Created by 이호영 on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
