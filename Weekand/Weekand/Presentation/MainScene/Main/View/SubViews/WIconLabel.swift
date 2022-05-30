0b4181e1aed4f91386db71ede89b8d310b422668
ChoiysApple <chcs1370@gmail.com>
2022-05-30 15:31:18 +0900

    Create WIconLabel

---
+++ Weekand/Weekand/Presentation/MainScene/Main/View/SubViews/WIconLabel.swift
@@ -0,0 +1,58 @@
+//
+//  WLeftIconLabel.swift
+//  Weekand
+//
+//  Created by Daegeon Choi on 2022/05/30.
+//  https://github.com/ChoiysApple/MVVMovie/blob/main/MovieApp/MovieApp/Presentation/DetailView/View/CustomView/IconLabel.swift
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class WIconLabel: UIView {
+    
+    lazy var icon = UIImageView().then {
+        $0.setContentHuggingPriority(.required, for: .vertical)
+    }
+    lazy var label = UILabel().then {
+        $0.textAlignment = .left
+    }
+    lazy var stack = UIStackView().then {
+        
+        $0.addArrangedSubview(icon)
+        $0.addArrangedSubview(label)
+
+        icon.snp.makeConstraints { make in
+            make.height.equalTo(15)
+            make.width.equalTo(icon.snp.height)
+        }
+        
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .fill
+        $0.spacing = 3
+    }
+    
+    //initWithFrame to init view from code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    //initWithCode to init view from xib or storyboard
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    //common func to init our view
+    private func setupView() {
+      self.addSubview(stack)
+      
+      stack.snp.makeConstraints { make in
+          make.edges.equalToSuperview()
+      }
+    }
+    
+}
