d385395fc6da8a74b14f356a59970cad16d4c400
ChoiysApple <chcs1370@gmail.com>
2022-05-30 14:44:44 +0900

    DesignSystem Font 추가

---
+++ Weekand/Weekand/Presentation/MainScene/Main/View/MainTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MainTableViewCell.swift
+//  Weekand
+//
+//  Created by Daegeon Choi on 2022/05/30.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
