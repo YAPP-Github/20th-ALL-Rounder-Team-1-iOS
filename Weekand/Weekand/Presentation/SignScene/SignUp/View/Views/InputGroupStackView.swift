5782283708d3a968c45e10e5f8f576b943eb0663
llghdud921 <llghdud921@naver.com>
2022-06-06 19:25:18 +0900

    feat: InformationGroupStackView 내 InformType 추가하여 중복 코드 개선

--- Weekand/Weekand/Presentation/SignScene/SignUp/View/Views/InputGroupStackView.swift
+++ Weekand/Weekand/Presentation/SignScene/SignUp/View/Views/InputGroupStackView.swift
@@ -10,6 +10,23 @@ import Then
 import SnapKit
 
 class InputGroupStackView: UIStackView {
+    
+    enum InformType {
+        case normal
+        case vaild
+        case invaild
+        
+        var textColor: UIColor {
+            switch self {
+            case .normal:
+                return .gray500 ?? .gray
+            case .vaild:
+                return .wblue ?? .systemBlue
+            case .invaild:
+                return .wred ?? .systemRed
+            }
+        }
+    }
 
     lazy var informationStackView = UIStackView().then {
         $0.axis = .horizontal
@@ -21,9 +38,7 @@ class InputGroupStackView: UIStackView {
         $0.textColor = UIColor.gray800
     }
     
-    lazy var informlabel = WTextLabel().then {
-        $0.textColor = UIColor.gray500
-    }
+    let informlabel = WTextLabel()
     
     let buttonTextField = WButtonTextField()
     
@@ -46,14 +61,18 @@ class InputGroupStackView: UIStackView {
         
         namelabel.setContentHuggingPriority(.init(rawValue: 251), for: .horizontal)
     }
-    
+
+}
+
+extension InputGroupStackView {
     func setNameLabelText(string: String) {
         namelabel.text = string
     }
     
-    func setInformlabelText(string: String) {
+    func setInformlabelText(string: String, informType: InformType) {
         informlabel.isHidden = false
         informlabel.text = string
+        informlabel.textColor = informType.textColor
     }
     
     func setPlaceholderText(string: String) {
@@ -63,21 +82,17 @@ class InputGroupStackView: UIStackView {
     func setButtonText(string: String) {
         buttonTextField.button.setTitle(string, for: .normal, font: WFont.body2())
     }
-    
-    // 개선 필요
+
     func hideTextFieldButton() {
         buttonTextField.button.isHidden = true
     }
     
-    func informInvaildMessage(string: String) {
-        informlabel.isHidden = false
-        informlabel.textColor = .wred
-        informlabel.text = string
+    func hideInformlabel() {
+        informlabel.isHidden = true
     }
     
-    func informVaildMessage(string: String) {
-        informlabel.isHidden = false
-        informlabel.textColor = .wblue
-        informlabel.text = string
+    func setSecureTextEntry() {
+        buttonTextField.textField.isSecureTextEntry = true
     }
+    
 }
