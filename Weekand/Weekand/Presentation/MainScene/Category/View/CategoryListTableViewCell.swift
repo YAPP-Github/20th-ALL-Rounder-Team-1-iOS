2a83339a172c310677592faf2df54bd34630f891
llghdud921 <llghdud921@naver.com>
2022-06-08 01:13:21 +0900

    feat: 카테고리 리스트 구현 #39

--- Weekand/Weekand/Presentation/MainScene/Category/View/CategoryListTableViewCell.swift
+++ Weekand/Weekand/Presentation/MainScene/Category/View/CategoryListTableViewCell.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class CategoryListTableViewCell: UITableViewCell {
 
+    static let cellIdentifier = "CategoryCell"
+    
+    lazy var cellStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .fill
+        $0.alignment = .center
+        $0.spacing = 12
+    }
+    
+    lazy var colorView = UIView().then {
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var contentStack = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .fill
+        $0.spacing = 6
+    }
+    
+    lazy var openTypeLabel = UILabel().then {
+        $0.font = WFont.body3()
+        $0.textColor = .gray500
+    }
+    
+    lazy var nameLabel = UILabel().then {
+        $0.font = WFont.subHead2()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -17,4 +45,46 @@ class CategoryListTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
     }
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        configureUI()
+        setupView()
+    }
+    
+    private func setupView() { }
+    
+    private func configureUI() {
+        
+        self.addSubview(cellStack)
+        
+        cellStack.addArrangedSubview(colorView)
+        cellStack.addArrangedSubview(contentStack)
+        
+        cellStack.snp.makeConstraints { make in
+            make.edges.equalTo(UIEdgeInsets(top: 12, left: 24, bottom: 12, right: 14))
+            make.height.equalTo(cellStack.snp.width).multipliedBy(1/5.4)
+        }
+        
+        colorView.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+            make.centerY.equalTo(self.snp.centerY)
+        }
+        
+        contentStack.addArrangedSubview(openTypeLabel)
+        contentStack.addArrangedSubview(nameLabel)
+    }
+    
+    public func configure(color: UIColor, openType: OpenType, name: String) {
+        colorView.backgroundColor = color
+        openTypeLabel.text = openType.description
+        nameLabel.text = name
+    }
+    
 }
--- Weekand/Weekand/Presentation/MainScene/Category/View/CategoryViewController.swift
+++ Weekand/Weekand/Presentation/MainScene/Category/View/CategoryViewController.swift
@@ -5,9 +5,31 @@
 //  Created by 이호영 on 2022/06/07.
 //
 
-import Foundation
+import UIKit
+import SnapKit
 
-class CategoryViewController: BaseViewController {
+class CategoryViewController: UIViewController {
+    
+    enum Section {
+      case main
+    }
+    
+    let sample: [Category] = [
+        Category(color: "red", name: "카테고리1", openType: .allOpen),
+        Category(color: "red", name: "카테고리2", openType: .closed),
+        Category(color: "red", name: "카테고리3", openType: .followerOpen),
+        Category(color: "red", name: "카테고리4", openType: .closed),
+        Category(color: "red", name: "카테고리5", openType: .allOpen),
+        Category(color: "red", name: "카테고리6", openType: .allOpen),
+        Category(color: "red", name: "카테고리7", openType: .closed),
+        Category(color: "red", name: "카테고리8", openType: .followerOpen),
+        Category(color: "red", name: "카테고리9", openType: .closed),
+        Category(color: "red", name: "카테고리10", openType: .allOpen)
+    ]
+    
+    var dataSource: UITableViewDiffableDataSource<Section, Category>!
+    
+    lazy var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,19 +37,51 @@ class CategoryViewController: BaseViewController {
         setupView()
         configureUI()
         bindViewModel()
+        configureDataSource()
+        configureSnapshot()
     }
     
     private func setupView() {
-        
+        tableView.separatorStyle = .none
+        tableView.register(CategoryListTableViewCell.self, forCellReuseIdentifier: CategoryListTableViewCell.cellIdentifier)
     }
     
     private func configureUI() {
-        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(self.view.safeAreaLayoutGuide)
+            make.trailing.leading.equalToSuperview()
+        }
     }
     
     private func bindViewModel() {
         
     }
+}
+
+// MARK: TableView
+extension CategoryViewController {
+    
+    private func configureDataSource() {
+        
+        dataSource = UITableViewDiffableDataSource<Section, Category>(tableView: tableView, cellProvider: { tableView, indexPath, list in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CategoryListTableViewCell.cellIdentifier, for: indexPath) as? CategoryListTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.accessoryType = .disclosureIndicator
+            cell.configure(color: UIColor(hex: "#FFC8C8") ?? .red, openType: list.openType, name: list.name)
+            return cell
+        })
+    }
+    
+    private func configureSnapshot(animatingDifferences: Bool = true) {
+
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Category>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(sample, toSection: .main)
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+    
 }
 
 import SwiftUI
@@ -36,8 +90,7 @@ import SwiftUI
 struct CategoryViewControllerPreview: PreviewProvider {
     static var previews: some View {
         Group {
-            CategoryViewController().showPreview(.iPhone8)
-            CategoryViewController().showPreview(.iPhone12Mini)
+            CategoryViewController().showPreview(.iPhone11Pro)
         }
     }
 }
