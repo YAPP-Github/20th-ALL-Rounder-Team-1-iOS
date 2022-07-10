5e907153856cffdd74e65ff881fe6a0f17bc213f
llghdud921 <llghdud921@naver.com>
2022-07-10 23:56:41 +0900

    feat: 회원가입 실패 시 popup띄우도록 구현 #55

--- Weekand/Weekand/Presentation/SignScene/SignUp/ViewModel/SignUpTermsViewModel.swift
+++ Weekand/Weekand/Presentation/SignScene/SignUp/ViewModel/SignUpTermsViewModel.swift
@@ -48,10 +48,16 @@ extension SignUpTermsViewModel {
                 if isSucceed {
                     self.coordinator?.finish()
                 } else {
-                    // error 처리
+                    self.coordinator?.presentPopViewController(
+                                        titleText: "오류",
+                                        informText: "회원가입에 실패하였습니다.",
+                                        dismissParentCoordinator: false)
                 }
             }, onFailure: { _ in
-                // error 처리
+                self.coordinator?.presentPopViewController(
+                                    titleText: "오류",
+                                    informText: "회원가입에 실패하였습니다.",
+                                    dismissParentCoordinator: false)
             }, onDisposed: nil)
             .disposed(by: disposeBag)
 
