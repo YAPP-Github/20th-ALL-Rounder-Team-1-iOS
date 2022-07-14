22d58e0e5f0a94e1019f33afa1bf7b9073ae0ba6
llghdud921 <llghdud921@naver.com>
2022-07-14 23:23:54 +0900

    feat: CategoryUseCase 내 ScheduleCategory fetch 메서드 작성 #79

--- Weekand/Weekand/Domain/UseCase/Main/CategoryUseCase.swift
+++ Weekand/Weekand/Domain/UseCase/Main/CategoryUseCase.swift
@@ -4,9 +4,15 @@
 //
 //  Created by 이호영 on 2022/07/12.
 //
-
 import Foundation
+import RxSwift
+
+typealias Categories = [ScheduleCategoriesQuery.Data.ScheduleCategory.ScheduleCategory]
 
 final class CategoryUseCase {
-    
+    func ScheduleCategories(sort: ScheduleSort, page: Int, size: Int) -> Single<Categories> {
+        NetWork.shared.fetch(query: ScheduleCategoriesQuery(sort: sort.toModel(), page: page, size: size))
+            .map { $0.scheduleCategories.scheduleCategories }
+            .asSingle()
+    }
 }
