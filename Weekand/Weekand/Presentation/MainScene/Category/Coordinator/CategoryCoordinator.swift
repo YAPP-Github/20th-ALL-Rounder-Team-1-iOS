b013376475f7cc3ac1b3a8e0c395aa2433820084
llghdud921 <llghdud921@naver.com>
2022-07-15 21:14:03 +0900

    refactor: categoryList 호출 두번 되는 버그 개선 #79

--- Weekand/Weekand/Presentation/MainScene/Category/Coordinator/CategoryCoordinator.swift
+++ Weekand/Weekand/Presentation/MainScene/Category/Coordinator/CategoryCoordinator.swift
@@ -60,9 +60,6 @@ extension CategoryCoordinator: CoordinatorDidFinishDelegate {
     
     func childDidFinish(_ child: Coordinator) {
         self.childCoordinators = self.childCoordinators.filter({ $0.type != child.type })
-        if child.type == .categoryAdd || child.type == .categoryModify {
-            self.categoryListViewController.setCategoryList()
-        }
         navigationController.dismiss(animated: true, completion: nil)
     }
 }
