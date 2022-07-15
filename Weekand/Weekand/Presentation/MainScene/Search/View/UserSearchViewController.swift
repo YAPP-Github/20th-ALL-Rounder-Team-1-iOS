473c40ca23b19765397635242508294ec7eeff66
llghdud921 <llghdud921@naver.com>
2022-07-15 22:03:24 +0900

    refactor: UserSearchViewController 네이밍 변경 #80

---
+++ Weekand/Weekand/Presentation/MainScene/Search/View/UserSearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  Weekand
+//
+//  Created by 이호영 on 2022/07/15.
+//
+
+import UIKit
+
+class UserSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
