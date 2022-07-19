e41db7f24b4539a1af034f9dd7615f6d1c804b88
llghdud921 <llghdud921@naver.com>
2022-07-19 19:55:18 +0900

    feat: CategoryError 생성 #79

--- Weekand/Weekand/Networking/Error/CategoryError.swift
+++ Weekand/Weekand/Networking/Error/CategoryError.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+enum CategoryError: LocalizedError {
+    case duplicatedName
+    case minimumCategoryCount
+    
+    var errorDescription: String? {
+        switch self {
+        case .minimumCategoryCount:
+            return "최소 2개 이상의 카테고리가 존재할 시 삭제 가능합니다"
+        case .duplicatedName:
+            return "해당 카테고리명은 이미 사용중입니다."
+        }
+    }
+}
