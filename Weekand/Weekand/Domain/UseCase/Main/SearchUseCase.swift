4aab54c2cb0dd83afd6a39ea9609e75000244a80
llghdud921 <llghdud921@naver.com>
2022-07-20 11:14:52 +0900

    fix: 네트워크 재요청 시 똑같이 반환하는 문제 개선

--- Weekand/Weekand/Domain/UseCase/Main/SearchUseCase.swift
+++ Weekand/Weekand/Domain/UseCase/Main/SearchUseCase.swift
@@ -11,7 +11,10 @@ import RxSwift
 final class SearchUseCase {
     func SearchUsers(searchQuery: String, jobs: [String], interests: [String], sort: UserSort, page: Int, size: Int)
     -> Single<SearchUsersQuery.Data.SearchUser> {
-                NetWork.shared.fetch(query: SearchUsersQuery(searchQuery: searchQuery, jobs: jobs, interests: interests, sort: sort.toModel(), page: page, size: size))
+                NetWork.shared.fetch(
+                    query: SearchUsersQuery(searchQuery: searchQuery, jobs: jobs, interests: interests, sort: sort.toModel(), page: page, size: size),
+                    cachePolicy: .fetchIgnoringCacheCompletely,
+                    queue: DispatchQueue.main)
             .map { $0.searchUsers }
             .asSingle()
         }
