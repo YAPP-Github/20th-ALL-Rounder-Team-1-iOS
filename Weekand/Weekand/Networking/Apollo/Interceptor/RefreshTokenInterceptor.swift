91f0157b314906d828dd49615305adc84fdac1f5
llghdud921 <llghdud921@naver.com>
2022-07-30 12:54:43 +0900

    refactor: network 파일들 폴더 정리

---
+++ Weekand/Weekand/Networking/Apollo/Interceptor/RefreshTokenInterceptor.swift
@@ -0,0 +1,34 @@
+//
+//  RefreshTokenInterceptor.swift
+//  Weekand
+//
+//  Created by 이호영 on 2022/07/24.
+//
+
+import Apollo
+import RxSwift
+
+class RefreshTokenInterceptor: ApolloInterceptor {
+    
+    func interceptAsync<Operation>(chain: RequestChain, request: HTTPRequest<Operation>, response: HTTPResponse<Operation>?, completion: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) where Operation : GraphQLOperation {
+        if let response = response {
+            
+            if let error = response.parsedResponse?.errors?.first,
+               error.description == NetworkError.expiredToken.errorDescription {
+                TokenManager.shared.isExpired = true
+                TokenManager.shared.reissue { result in
+                    switch result {
+                    case .success:
+                        chain.retry(request: request, completion: completion)
+                    case .failure(let error):
+                        chain.handleErrorAsync(error, request: request, response: response, completion: completion)
+                    }
+                }
+            } else {
+                chain.proceedAsync(request: request,
+                                   response: response,
+                                   completion: completion)
+            }
+        }
+    }
+}
