2b366b7e9ffd2521ec02f1d377a6c7b11ff802b3
ChoiysApple <chcs1370@gmail.com>
2022-08-04 10:30:01 +0900

    fix: SplashScreen 1초로 수정

--- Weekand/Weekand/Application/AppDelegate.swift
+++ Weekand/Weekand/Application/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 1.0)
         return true
     }
 
